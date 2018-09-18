9afb96b9646045fcc10141603771448bd402603f
徐何君 <1124418652@qq.com>
2018-09-18 22:30:47 +0800

    add the defination of function gray_separate(img)

--- Chapter_3/gray_scale_transform.py
+++ Chapter_3/gray_scale_transform.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-# project: the algrithom of gray scale transfrom 
+# project: the algrithom of gray scale transfrom
 # author:  xhj
 # email:   1124418652@qq.com
 # date:    2018/ 9/18
@@ -10,7 +10,7 @@ import os
 import sys
 import time
 import numpy as np
-import matplotlib.pyplot as plt  
+import matplotlib.pyplot as plt
 from PIL import *
 from math import pow
 
@@ -22,11 +22,11 @@ def open_img(path, type = "RGB"):
 	if "RGB" != type.upper():
 		img = img.convert("L")
 
-	return img 
+	return img
 
 def gama_trans(img, y = 0.4):
 	c = 255 / (pow(255, y))
-	
+
 	if type(np.array([1, 2])) != type(img):
 		img = np.array(img)
 
@@ -37,7 +37,7 @@ def gama_trans(img, y = 0.4):
 		for w in range(width):
 			res[h][w] = c * pow(img[h][w], y)
 
-	return res 
+	return res
 
 def separate_trans(img, r1 = 0, s1 = 0, r2 = 255, s2 = 255):
 	if type(np.array([1])) != type(img):
@@ -59,7 +59,19 @@ def separate_trans(img, r1 = 0, s1 = 0, r2 = 255, s2 = 255):
 			else:
 				res[h][w] = int((255 - s2) / (255 - r1) * _tmp + r2)
 
-	return res 
+	return res
+
+def gray_separate(img):
+    if type(np.array([1])) != type(img):
+        img = np.array(img)
+
+    height, width = img.shape
+    res = np.zeros((8, height, width))
+
+    for layer in np.linspace(1, 8, 8):
+        for h in range(height):
+            for w in range(width):
+
 
 def show_img(img):
 	if type(np.array([1, 2])) == type(img):
