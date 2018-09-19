c8a42e9af6a5b001df79d8b8e7dfae5ed324a46f
hejun <1124418652@qq.com>
2018-09-19 11:44:07 +0800

    add the function hist_equal(img)

--- Chapter_3/gray_scale_transform.py
+++ Chapter_3/gray_scale_transform.py
@@ -13,6 +13,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import *
 from math import pow
+from scipy.interpolate import interp1d
 
 dir_path = os.path.join(os.path.abspath('.'), "../src/")
 
@@ -64,7 +65,7 @@ def separate_trans(img, r1 = 0, s1 = 0, r2 = 255, s2 = 255):
 	return res
 
 # 比特平面分层
-def num2bit(num):
+def _num2bit(num):
 	b = np.zeros(8)
 	i = 0
 
@@ -86,14 +87,43 @@ def gray_separate(img):
         for h in range(height):
             for w in range(width):
             	num = img[h][w]
-            	res[layer][h][w] = num2bit(num)[layer] * 128
+            	res[layer][h][w] = _num2bit(num)[layer] * 128
 
     fig = plt.figure()
     for layer in range(8):
     	img = Image.fromarray(res[layer])
     	ax = fig.add_subplot(2, 4, layer + 1)
+    	title = "layer: " + str(layer)
+    	ax.set_title(title)
     	ax.imshow(img)
     plt.show()
+
+# histogram equalization
+def hist_equal(img):
+	if type(np.array([1,2])) != img:
+		img = np.array(img)
+
+	if 2 == len(img.shape):          # 单通道图像
+		accumulate = np.zeros(256)
+		height, width = img.shape
+		img = img.flatten()
+		
+		for val in img:
+			accumulate[val] += 1 
+
+		cum = accumulate.cumsum()
+		cum = cum / cum[-1] * 255
+		f = interp1d(range(256), cum, kind="linear")
+		im2 = f(img)
+
+		fig = plt.figure()
+		ax1 = fig.add_subplot(131)
+		ax1.plot(range(256), accumulate)
+		ax2 = fig.add_subplot(132)
+		ax2.imshow(Image.fromarray(img.reshape((height, width))))
+		ax3 = fig.add_subplot(133)
+		ax3.imshow(Image.fromarray(im2.reshape(height, width)))
+		plt.show()
 
 def show_img(img):
 	if type(np.array([1, 2])) == type(img):
@@ -105,7 +135,8 @@ def show_img(img):
 def main():
 	path = os.path.join(dir_path, "lena.jpg")
 	img = open_img(path, "gray")
-	gray_separate(img)
+	# gray_separate(img)
+	hist_equal(img)
 
 if __name__ == '__main__':
 	main()
