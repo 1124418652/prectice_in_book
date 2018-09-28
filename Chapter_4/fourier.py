d845a17b5d96cfc1294adb3a1cec552c26ef27c9
hejun <1124418652@qq.com>
2018-09-28 11:51:13 +0800

    plot the one dimension fourier transform

--- Chapter_4/fourier.py
+++ Chapter_4/fourier.py
@@ -1,49 +1,64 @@
-# -*- coding: utf-8 -*-
-"""
-# project: Fourier Transformation
-# author: xhj
-# email: 1124418652@qq.com
-# date: 2018 9/22
-"""
-
 import numpy as np
-from PIL import Image
-from math import exp, cos, pi
 import matplotlib.pyplot as plt
+from pylab import mpl
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+mpl.rcParams['axes.unicode_minus'] = False
+from math import pi, cos, sin, pow
 
-def open_img(path, mode = "gray"):
-    img = Image.open(path)
+a = np.zeros(100)
+a[40: 60] = 1
 
-    if "gray" == mode.lower():
-        img = img.convert("L")
+def fourier(fx):
+    num = len(fx)
+    Fu_real = np.zeros(num)
+    Fu_image = np.zeros(num)
+    Fu_value = np.zeros(num)
+    
+    for u in range(num):
+        for x in range(num):
+            Fu_real[u] += fx[x] * cos(-2 * pi * x / num * u) * pow(-1, x)    # 将 fx 乘以 pow(-1,x) 可以将傅里叶变换的低频移到图片中间
+            #print("\tu=", u, "x=", x, "\t", x*cos(2 * pi * x / num * u))
+            Fu_image[u] -= fx[x] * sin(2 * pi * u / num * x) * pow(-1, x)
+        Fu_value[u] = pow(pow(Fu_real[u], 2) + pow(Fu_image[u], 2), 0.5)
 
-    return img
+    return Fu_value, Fu_real, Fu_image
 
-def fourier_trans(img):
-    if type(np.array([1,2])) != type(img):
-        img = np.array(img)
+def infourier(Fu_real, Fu_image):
+    num = len(Fu_real)
+    fx = np.zeros(num)
+    
+    for x in range(num):
+        for u in range(num):
+            fx[x] += 1 / num * (Fu_real[u] * cos(2 * pi * x / num * u) - Fu_image[u] * sin(2 * pi * u / num * x)) * pow(-1, x)
+    
+    return fx
 
-    height, width = img.shape
-    res = np.zeros((height, width))
+Fu_value, Fu_real, Fu_image = fourier(a)
+fx = infourier(Fu_real, Fu_image)
+#print(fx)
 
-    for u in [int(height/2)]:
-        for v in range(width):
-            for h in [int(height/2)]:
-                for w in range(width):
-                    res[u][v] += img[h][w] * cos(2 * pi * (u * h / height + v * w / width))
+fig = plt.figure()
 
-    return res
+#绘制原函数的图像
+ax1 = fig.add_subplot(221)
+ax1.plot(a)
+ax1.set_title(u"原函数")
+ax1.set_xlabel(u"x")
+ax1.set_ylabel(u"f(x)")
 
-def main():
-    path = "src/timg.jpg"
-    img1 = open_img(path)
-    res = fourier_trans(img1)
-    fig = plt.figure()
-    ax1 = fig.add_subplot(121)
-    ax1.imshow(img1)
-    ax2 = fig.add_subplot(122)
-    ax2.imshow(Image.fromarray(res))
-    plt.show()
+#绘制傅里叶变换的谱图
+ax2 = fig.add_subplot(222)
+ax2.plot(Fu_value)
+ax2.set_title(u"傅里叶变换谱图")
+ax2.set_xlabel(u"u")
+ax2.set_ylabel(u"|F(u)|")
 
-if __name__ == '__main__':
-    main()
+#绘制傅里叶逆变换的图像
+ax3 = fig.add_subplot(223)
+ax3.plot(fx)
+ax3.set_title(u"傅里叶逆变换")
+ax3.set_xlabel(u"x")
+ax3.set_ylabel(u"fx")
+
+plt.tight_layout()        #设置子图之间的间距
+plt.show()
